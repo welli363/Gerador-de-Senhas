0bcbb44ce3d1c34595e4ec7cfe467c931f14d967
Wellington da Costa <toncosta08@gmail.com>
2025-08-24 21:06:42 -0300

    adicionei novas funcionalidades e novas mensagens ao gerador de senhas
    foi adicionado novas funcionalidades e mensagens ao gerador de senhas.

--- index.js
+++ index.js
@@ -3,8 +3,35 @@
 //mostrar a senha no input 
 //personalizar o tamanho da senha 
 
+const msgForca = document.querySelector('#msgForca');
+
+function forcaSenha(senhaGerada){
+const tamanho = senhaGerada.length;
+
+if(tamanho <=10){
+    msgForca.textContent = 'Senha fraca';
+    msgForca.style.color = 'red';
+}
+else if(tamanho >= 10 && tamanho <= 20){
+    msgForca.textContent = 'Senha mediana';
+    msgForca.style.color = 'yellow';
+}
+else if(tamanho > 20){
+    msgForca.textContent = 'Senha forte';
+    msgForca.style.color = 'green';
+};
+    //tamnho da frase
+    msgForca.style.fontSize = '16px';
+    msgForca.style.fontWeight = 'bold'
+    //a mensagem irá sumir depois de um tempo
+    setTimeout(()=>{
+         msgForca.textContent =''; //limpa a mensagem de copiado
+    }, 5000);
+};
+
 const tamanhoSenha = document.querySelector('#tamanho')
 const botaoTamanho = document.querySelector('#gerarTamanho');
+const inputSenha = document.querySelector('#senha');
 
 botaoTamanho.addEventListener('click', function(){
     const tamanho = Number(tamanhoSenha.value) || 15;
@@ -14,15 +41,13 @@ botaoTamanho.addEventListener('click', function(){
     }
     let senhaGerada = gerarSenha(tamanho);
     inputSenha.value = senhaGerada
+
+    //veriocar força da senha
+    forcaSenha(senhaGerada);
 });
 
 
-
-
-const inputSenha = document.querySelector('#senha');
-const botaoGerar = document.querySelector('#gerador');
-
-function gerarSenha( tamanho = 15){
+    function gerarSenha( tamanho = 15){
     const caracteres = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%&*-_.,:;?/';
     let senha = '';
     
@@ -33,12 +58,13 @@ function gerarSenha( tamanho = 15){
     return senha;
 }
 
+
 const botaoCopiar =document.querySelector('#copiar');
 const msgCopiada = document.querySelector('.MsgCopiada');
 botaoCopiar.addEventListener('click', function(){
     navigator.clipboard.writeText(inputSenha.value);
     msgCopiada.textContent = 'Copiado com sucesso!'
-    msgCopiada.style.color = 'black';
+    msgCopiada.style.color = 'lightgreen';
     msgCopiada.style.fontWeight = 'bold';
     msgCopiada.style.fontSize = '16px';
     msgCopiada.style.margin = '10px';
